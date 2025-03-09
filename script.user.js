63fa6a41c2f95b5ede1cb29d6c20d6131f47b72b
Michael Ross Tarr <mrmrmtarr@gmail.com>
2025-03-09 20:28:27 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME EZSegments
 // @namespace    https://greasyfork.org/en/scripts/518381-wme-ezsegments
-// @version      1.24
+// @version      1.25
 // @description  Easily update roads
 // @author       https://github.com/michaelrosstarr
 // @include 	 /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
@@ -218,7 +218,7 @@ const handleUpdate = () => {
 
         // Set lock if enabled
         if (options.setLock) {
-            const rank = wmeSDK.State.getUserInfo().rank + 1;
+            const rank = wmeSDK.State.getUserInfo().rank;
             const selectedRoad = roadTypes.find(rt => rt.value === options.roadType);
             if (selectedRoad) {
                 const lockSetting = options.locks.find(l => l.id === selectedRoad.id);
